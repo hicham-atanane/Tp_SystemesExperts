9534343fe1e636fe54b5ca8865f0437bdd7aa3ce
Hicham Atanane <hicham.atn@gmail.com>
2022-05-30 20:51:06 +0100

    Tp1 - Tp2 version finale

--- src/exercice1/Main.java
+++ src/exercice1/Main.java
@@ -6,7 +6,7 @@ import exercice2.RegleComposee;
 
     public class Main {
         public static void main(String[] args) {
-    //Exercice1
+//  Exercice1 ---------------------------------------------------------------------------------------------------
             RegleSimple r1 = new RegleSimple("P","Q");
             RegleSimple r2 = new RegleSimple("Q","R");
             RegleSimple r3 = new RegleSimple("T","R");
@@ -19,9 +19,9 @@ import exercice2.RegleComposee;
             bR1.add(r3);
 
             bF1.add("P");
-            System.out.println("Exercice1");
-            System.out.println(MoteurInference.chainageAvant(bF1, bR1, "R"));
-    //Exercice2
+            System.out.println("Exercice1 TP1");
+            System.out.println(MoteurInference.chainageAvant(bF1, bR1, "F"));
+//  Exercice2 TP1 - TP2 ------------------------------------------------------------------------------------
             RegleComposee rc_1 = new RegleComposee("A&B","C");
             RegleComposee rc_2 = new RegleComposee("C&D","F");
             RegleComposee rc_3 = new RegleComposee("F&B","E");
@@ -41,50 +41,51 @@ import exercice2.RegleComposee;
             bF2.add("C");
             bF2.add("D");
 
-            System.out.println("Exercice2");
+            System.out.println("Exercice2 Chainage Avant");
             System.out.println(MoteurInference.chainageAvantComp(bF2, bR2, "E"));
+            System.out.println("Exercice2 Chainage Arriere");
+            System.out.println(MoteurInference.chainageArriereComp(bF2, bR2, "E"));
 
-//Exercice3
-        RegleComposee rc1  = new RegleComposee("fleur&graine","phanérogame");
-        RegleComposee rc2  = new RegleComposee("phanérogame&graine_nue","sapin");
-        RegleComposee rc3  = new RegleComposee("phanérogame&1_cotylédone","monocotylédone");
-        RegleComposee rc4  = new RegleComposee("phanérogame&2_cotylédone","dicotylédone");
-        RegleComposee rc5  = new RegleComposee("monocotylédone&rhyzome","muguet");
-        RegleComposee rc6  = new RegleComposee("dicotylédone","anémone");
-        RegleComposee rc7  = new RegleComposee("monocotylédone&non_rhyzome","lilas");
-        RegleComposee rc8  = new RegleComposee("feuille&fleur","cryptogame");
-        RegleComposee rc9  = new RegleComposee("cryptogame&non_racine","mousse");
-        RegleComposee rc10 = new RegleComposee("cryptogame&racine","fougère");
-        RegleComposee rc11 = new RegleComposee("non_feuille&plante","thallophyte");
-        RegleComposee rc12 = new RegleComposee("thallophyte&chlorophylle","algue");
-        RegleComposee rc13 = new RegleComposee("thallophyte&non_chlorophylle","champion");
-        RegleComposee rc14 = new RegleComposee("non_feuilles&non_fleur&non_plante","colibacile");
+//  Exercice3 ---------------------------------------------------------------------------------------------------
+            RegleComposee rc1  = new RegleComposee("fleur&graine","phanérogame");
+            RegleComposee rc2  = new RegleComposee("phanérogame&graine_nue","sapin");
+            RegleComposee rc3  = new RegleComposee("phanérogame&1_cotylédone","monocotylédone");
+            RegleComposee rc4  = new RegleComposee("phanérogame&2_cotylédone","dicotylédone");
+            RegleComposee rc5  = new RegleComposee("monocotylédone&rhyzome","muguet");
+            RegleComposee rc6  = new RegleComposee("dicotylédone","anémone");
+            RegleComposee rc7  = new RegleComposee("monocotylédone&non_rhyzome","lilas");
+            RegleComposee rc8  = new RegleComposee("feuille&fleur","cryptogame");
+            RegleComposee rc9  = new RegleComposee("cryptogame&non_racine","mousse");
+            RegleComposee rc10 = new RegleComposee("cryptogame&racine","fougère");
+            RegleComposee rc11 = new RegleComposee("non_feuille&plante","thallophyte");
+            RegleComposee rc12 = new RegleComposee("thallophyte&chlorophylle","algue");
+            RegleComposee rc13 = new RegleComposee("thallophyte&non_chlorophylle","champion");
+            RegleComposee rc14 = new RegleComposee("non_feuilles&non_fleur&non_plante","colibacile");
 
-        ArrayList<RegleComposee> bR3 = new ArrayList<>();
-        bR3.add(rc1);
-        bR3.add(rc2);
-        bR3.add(rc3);
-        bR3.add(rc4);
-        bR3.add(rc5);
-        bR3.add(rc6);
-        bR3.add(rc7);
-        bR3.add(rc8);
-        bR3.add(rc9);
-        bR3.add(rc10);
-        bR3.add(rc11);
-        bR3.add(rc12);
-        bR3.add(rc13);
-        bR3.add(rc14);
+            ArrayList<RegleComposee> bR3 = new ArrayList<>();
+            bR3.add(rc1);
+            bR3.add(rc2);
+            bR3.add(rc3);
+            bR3.add(rc4);
+            bR3.add(rc5);
+            bR3.add(rc6);
+            bR3.add(rc7);
+            bR3.add(rc8);
+            bR3.add(rc9);
+            bR3.add(rc10);
+            bR3.add(rc11);
+            bR3.add(rc12);
+            bR3.add(rc13);
+            bR3.add(rc14);
 
-        ArrayList<String> bF3 = new ArrayList<>();
-        bF3.add("non_feuille");
-        bF3.add("chlorophylle");
-        bF3.add("fleur");
-        bF3.add("graine");
-        bF3.add("plante");
+            ArrayList<String> bF3 = new ArrayList<>();
+            bF3.add("non_feuille");
+            bF3.add("chlorophylle");
+            bF3.add("fleur");
+            bF3.add("graine");
+            bF3.add("plante");
 
-        System.out.println("Exercice3");
-//        System.out.println(MoteurInference.chainageArriere(bF3, bR3, "fougère1"));
-        System.out.println(MoteurInference.chainageAvantComp(bF3, bR3));
-    }
+            System.out.println("Exercice3 Chainage Avant");
+            System.out.println(MoteurInference.chainageAvantComp(bF3, bR3));
+        }
 }
--- src/exercice1/MoteurInference.java
+++ src/exercice1/MoteurInference.java
@@ -26,29 +26,34 @@ public class MoteurInference {
     public static boolean chainageAvantComp(ArrayList<String> baseFaits, ArrayList<RegleComposee> baseReglesComp, String propVerif){
         ListIterator<RegleComposee> iter = baseReglesComp.listIterator();
 //      verifier que propVerif n'est pas dans baseFaits & baseRegles n'est pas vide
-            while (!baseFaits.contains(propVerif) && iter.hasNext()){
-                RegleComposee regle = iter.next();
+        while (!baseFaits.contains(propVerif) && iter.hasNext()){
+            RegleComposee regle = iter.next();
 //          verifier l'existence du premisse dans les bases des faits
-                int counter = 0;
-                for (int i = 0; i < regle.getPremisse().length; i++) {
-                    if (baseFaits.contains(regle.getPremisse()[i])){
-                        counter ++;
-                    }
-                }
-//          ajouter la conclusion aux baseFaits
-//          Supprimer la regle de la baseRegle (de iter en fait)
-                if (counter == regle.getPremisse().length){
-                    baseFaits.add(iter.next().getConclusion());
-                    iter.remove();
+            int counter = 0;
+            for (int i = 0; i < regle.getPremisse().length; i++) {
+                if (baseFaits.contains(regle.getPremisse()[i])){
+                    counter ++;
                 }
             }
+/*
+          ajouter la conclusion aux baseFaits
+          Supprimer la regle de la baseRegle (de iter en fait)
+*/
+            if (counter == regle.getPremisse().length){
+                baseFaits.add(regle.getConclusion());
+                iter.remove();
+            }
+        }
+        if (!baseFaits.contains(propVerif) && !baseReglesComp.isEmpty()){
+            chainageAvantComp(baseFaits, baseReglesComp, propVerif);
+        }
 //          vérifier si elle s’est arrêtée parce que la propVerif est retrouvée dans la liste des baseFaits ou non
         return baseFaits.contains(propVerif);
-        }
+    }
     public static ArrayList<String> chainageAvantComp(ArrayList<String> baseFaits, ArrayList<RegleComposee> baseReglesComp){
         ListIterator<RegleComposee> iter = baseReglesComp.listIterator();
 //      verifier que propVerif n'est pas dans baseFaits & baseRegles n'est pas vide
-            while (iter.hasNext()){
+        while (iter.hasNext()){
                 RegleComposee regle = iter.next();
 //          verifier l'existence du premisse dans les bases des faits
                 int counter = 0;
@@ -57,21 +62,23 @@ public class MoteurInference {
                         counter ++;
                     }
                 }
-//          ajouter la conclusion aux baseFaits
-//          Supprimer la regle de la baseRegle (de iter en fait)
-                if (counter == regle.getPremisse().length){
-                    baseFaits.add(regle.getConclusion());
+/*
+          ajouter la conclusion aux baseFaits
+          Supprimer la regle de la baseRegle
+*/
+            if (counter == regle.getPremisse().length){
+                    if (!baseFaits.contains(regle.getConclusion())){
+                        baseFaits.add(regle.getConclusion());
+                    }
                     iter.remove();
                 }
             }
         return baseFaits;
         }
-    public static boolean chainageArriere(ArrayList<String> baseFaits, ArrayList<RegleComposee> baseReglesComp, String propVerif){
+    public static boolean chainageArriereComp(ArrayList<String> baseFaits, ArrayList<RegleComposee> baseReglesComp, String propVerif){
 //      vérifier si la PropVerif figure dans la BF, si oui, elle doit retourner true.
-        if (baseFaits.contains(propVerif)){
-            return true;
+        if (baseFaits.contains(propVerif)){}
 //      Sinon, il faut parcourir la BR et vérifier si la PropoVerif figure comme conclusion d’une des règles
-        }
         else {
             ListIterator<RegleComposee> iter = baseReglesComp.listIterator();
             ArrayList<RegleComposee> reglesChoisis = new ArrayList<>();
@@ -82,23 +89,27 @@ public class MoteurInference {
                     reglesChoisis.add(regle);
                 }
             }
-//          Si la règle est trouvée alors la méthode « chainageArrière » doit être appliquée par
-//          récursivité sur la prémisse de la règle trouvée et retourner sa valeur de vérité
+/*
+          Si la règle est trouvée alors la méthode « chainageArrière » doit être appliquée par
+          récursivité sur la prémisse de la règle trouvée et retourner sa valeur de vérité
+*/
             if (!reglesChoisis.isEmpty()) {
                 for (RegleComposee r : reglesChoisis) {
                     for (String premisse : r.getPremisse()) {
-                        chainageArriere(baseFaits, baseReglesComp, premisse);
+                        chainageArriereComp(baseFaits, baseReglesComp, premisse);
                     }
-//          Si la règle n’est pas trouvée il faut préciser que la règle n’existe pas et que l’utilisateur
-//          doit préciser sa valeur de vérité
                 }
-            } else {
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("La règle n’existe pas. Prière de préciser sa valeur de vérité. :)");
-                    String valeur = sc.nextLine();
+            }
+/*
+            Si la règle n’est pas trouvée il faut préciser que la règle n’existe pas et que l’utilisateur
+            doit préciser sa valeur de vérité
+*/          else {
+//                    Scanner sc = new Scanner(System.in);
+//                    System.out.println("La règle n’existe pas. Prière de préciser sa valeur de vérité. :)");
+//                    String valeur = sc.nextLine();
                     return false;
                 }
-        return true;
         }
+        return true;
     }
 }
